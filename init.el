bff4e86ffe774a1070d3957efce5fe0101058373
Joshua Mathews <joshuamathews@Joshuas-MacBook-Pro-2.local>
2020-09-12 15:21:37 -0500

    initial commit

---
+++ init.el
@@ -0,0 +1,53 @@
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
+(package-refresh-contents)
+
+;; Download Evil
+(unless (package-installed-p 'evil)
+  (package-install 'evil))
+
+;; Enable Evil
+(require 'evil)
+(evil-mode 1)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (manoj-dark)))
+ '(package-selected-packages (quote (rainbow-delimiters geiser helm slime evil))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(setq inferior-lisp-program "/usr/local/bin/sbcl")
+
+(setq geiser-mit-binary "/usr/local/bin/scheme")
+
+(setq geiser-active-implementations '(mit))
+
+(require 'rainbow-delimiters)
+
+(mapcar (lambda (hook)
+	  (message "Adding rainbow delimiters mode to: %s" hook)
+	  (add-hook hook #'rainbow-delimiters-mode))
+	(list #'scheme-mode-hook #'emacs-lisp-mode-hook))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(rainbow-delimiters-depth-1-face ((t (:foreground "dark orange"))))
+ '(rainbow-delimiters-depth-2-face ((t (:foreground "deep pink"))))
+ '(rainbow-delimiters-depth-3-face ((t (:foreground "chartreuse"))))
+ '(rainbow-delimiters-depth-4-face ((t (:foreground "deep sky blue"))))
+ '(rainbow-delimiters-depth-5-face ((t (:foreground "yellow"))))
+ '(rainbow-delimiters-depth-6-face ((t (:foreground "orchid"))))
+ '(rainbow-delimiters-depth-7-face ((t (:foreground "spring green"))))
+ '(rainbow-delimiters-depth-8-face ((t (:foreground "sienna1")))))
